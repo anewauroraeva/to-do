f48651de4c084d92cda0c790205b0b556f20f15f
Eugenia <karandashevaa@gmail.com>
2024-03-11 19:46:15 +0300

    configurate webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  // entry: './src/index.js',
+  output: {
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+};
